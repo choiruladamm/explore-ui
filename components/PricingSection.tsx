de52dbe873e84e68c90179e7b8952ef7aeb0c2cc
choiruladamm <choiruladamm@gmail.com>
2023-08-18 10:49:54 +0700

    style: add react icons & finish pricing section

--- components/PricingSection.tsx
+++ components/PricingSection.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import React from "react";
+import { BsCheck2 } from "react-icons/bs";
 
 export function PricingSection() {
   return (
@@ -13,7 +16,74 @@ export function PricingSection() {
             quasi iusto modi velit ut non voluptas in. Explicabo id ut laborum.
           </p>
         </div>
+
+        <div className="max-w-2xl mx-auto mt-16 rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+          <div className="p-8">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900">
+              Lifetime membership
+            </h3>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque
+              amet indis perferendis blanditiis repellendus etur quidem
+              assumenda.
+            </p>
+            <div className="flex items-center mt-10 gap-x-4">
+              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+                What's Included
+              </h4>
+              <div className="flex-auto h-px bg-gray-200" />
+            </div>
+            <ul
+              role="list"
+              className="grid grid-cols-1 gap-4 mt-8 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+            >
+              {includedFeatures.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <BsCheck2
+                    className="flex-none w-5 h-6 fill-indigo-600"
+                    aria-hidden={true}
+                  />
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="p-2 -mt-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+            <div className="py-10 text-center rounded-xl bg-gray-50 ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+              <div className="max-w-xs px-8 mx-auto">
+                <p className="text-base font-semibold text-gray-600">
+                  Pay once, own it forever
+                </p>
+                <p className="flex items-baseline justify-center mt-6 gap-x-2">
+                  <span className="text-5xl font-bold tracking-tight text-gray-900">
+                    $349
+                  </span>
+                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
+                    USD
+                  </span>
+                </p>
+                <a
+                  href="#"
+                  className="block w-full px-3 py-2 mt-10 text-sm font-semibold text-center text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get access
+                </a>
+                <p className="mt-6 text-xs leading-5 text-gray-600">
+                  Invoices and receipts available for easy company reimbursement
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+const includedFeatures = [
+  "Private forum access",
+  "Member resources",
+  "Entry to annual conference",
+  "Official member t-shirt",
+];
